8f71ee3e0a29f76fe50d116b4c17616b776d9b65
Bruno Pavese <bruno@ejcm.com.br>
2022-08-20 16:47:54 -0300

    inicio aula forms

--- front/App.tsx
+++ front/App.tsx
@@ -1,11 +1,13 @@
-import Login from './src/pages/Login';
-import Pokédex from './src/pages/Pokédex';
-import { useFonts, Roboto_300Light, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, Roboto_900Black } from "@expo-google-fonts/roboto";
 import React from "react";
+
+import Login from './src/pages/Login';
 import AppLoading from "expo-app-loading";
-import MeusPokémons from './src/pages/MeusPokémons';
 import Perfil from './src/pages/Perfil';
 import EditarPerfil from './src/pages/EditarPerfil';
+import MeusPokémons from './src/pages/MeusPokémons';
+//import Pokédex from './src/pages/Pokédex';
+
+import { useFonts, Roboto_300Light, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, Roboto_900Black } from "@expo-google-fonts/roboto";
 
 
 
@@ -17,7 +19,7 @@ export default function App() {
   }
   return (
     <>
-      <EditarPerfil />
+      <Login />
 
     </>
 
